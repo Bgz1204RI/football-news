4f8244a878b04f564fed96798f789631729ec68b
Bgz1204RI <bagaszharif24@gmail.com>
2025-09-23 02:46:38 +0700

    tutorial 3 completed

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from main.views import register, logout_user, login_user   # ← add login_user
 from main.views import (
     show_main, create_news, show_news,
-    show_xml, show_json,            # you already have these
-    show_xml_by_id, show_json_by_id # new
+    show_xml, show_json,
+    show_xml_by_id, show_json_by_id
 )
 
 app_name = 'main'
@@ -12,11 +13,12 @@ urlpatterns = [
     path('create-news/', create_news, name='create_news'),
     path('news/<str:id>/', show_news, name='show_news'),
 
-    # list endpoints
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-
-    # by-id endpoints
     path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
+
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),            # ← add this line
+    path('logout/', logout_user, name='logout'),
 ]
